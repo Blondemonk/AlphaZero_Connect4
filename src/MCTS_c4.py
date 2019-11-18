87bc467320a7a5778ac8d4710582884fefa7709d
Mark <03phillipsm@gmail.com>
2019-11-18 21:02:16 +0000

    Update MCTS_c4.py

--- src/MCTS_c4.py
+++ src/MCTS_c4.py
@@ -36,13 +36,13 @@ class UCTNode():
         self.game = game # state s
         self.move = move # action index
         self.is_expanded = False
-        self.parent = parent  
+        self.parent = parent
         self.children = {}
         self.child_priors = np.zeros([7], dtype=np.float32)
         self.child_total_value = np.zeros([7], dtype=np.float32)
         self.child_number_visits = np.zeros([7], dtype=np.float32)
         self.action_idxes = []
-        
+
     @property
     def number_visits(self):
         return self.parent.child_number_visits[self.move]
@@ -50,22 +50,22 @@ class UCTNode():
     @number_visits.setter
     def number_visits(self, value):
         self.parent.child_number_visits[self.move] = value
-    
+
     @property
     def total_value(self):
         return self.parent.child_total_value[self.move]
-    
+
     @total_value.setter
     def total_value(self, value):
         self.parent.child_total_value[self.move] = value
-    
+
     def child_Q(self):
         return self.child_total_value / (1 + self.child_number_visits)
-    
+
     def child_U(self):
         return math.sqrt(self.number_visits) * (
             abs(self.child_priors) / (1 + self.child_number_visits))
-    
+
     def best_child(self):
         if self.action_idxes != []:
             bestmove = self.child_Q() + self.child_U()
@@ -73,21 +73,21 @@ class UCTNode():
         else:
             bestmove = np.argmax(self.child_Q() + self.child_U())
         return bestmove
-    
+
     def select_leaf(self):
         current = self
         while current.is_expanded:
           best_move = current.best_child()
           current = current.maybe_add_child(best_move)
         return current
-    
+
     def add_dirichlet_noise(self,action_idxs,child_priors):
         valid_child_priors = child_priors[action_idxs] # select only legal moves entries in child_priors array
         valid_child_priors = 0.75*valid_child_priors + 0.25*np.random.dirichlet(np.zeros([len(valid_child_priors)], \
                                                                                           dtype=np.float32)+192)
         child_priors[action_idxs] = valid_child_priors
         return child_priors
-    
+
     def expand(self, child_priors):
         self.is_expanded = True
         action_idxs = self.game.actions(); c_p = child_priors
@@ -98,11 +98,11 @@ class UCTNode():
         if self.parent.parent == None: # add dirichlet noise to child_priors in root node
             c_p = self.add_dirichlet_noise(action_idxs,c_p)
         self.child_priors = c_p
-    
+
     def decode_n_move_pieces(self,board,move):
         board.drop_piece(move)
         return board
-            
+
     def maybe_add_child(self, move):
         if move not in self.children:
             copy_board = copy.deepcopy(self.game) # make copy of board
@@ -110,7 +110,7 @@ class UCTNode():
             self.children[move] = UCTNode(
               copy_board, move, parent=self)
         return self.children[move]
-    
+
     def backup(self, value_estimate: float):
         current = self
         while current.parent is not None:
@@ -120,7 +120,7 @@ class UCTNode():
             elif current.game.player == 0: # same as current.parent.game.player = 1
                 current.total_value += (-1*value_estimate)
             current = current.parent
-        
+
 class DummyNode(object):
     def __init__(self):
         self.parent = None
@@ -132,7 +132,7 @@ def UCT_search(game_state, num_reads,net,temp):
     for i in range(num_reads):
         leaf = root.select_leaf()
         encoded_s = ed.encode_board(leaf.game); encoded_s = encoded_s.transpose(2,0,1)
-        encoded_s = torch.from_numpy(encoded_s).float().cuda()
+        encoded_s = torch.from_numpy(encoded_s).float()  #.cuda()
         child_priors, value_estimate = net(encoded_s)
         child_priors = child_priors.detach().cpu().numpy().reshape(-1); value_estimate = value_estimate.item()
         if leaf.game.check_winner() == True or leaf.game.actions() == []: # if somebody won or draw
@@ -153,10 +153,10 @@ def get_policy(root, temp=1):
 
 def MCTS_self_play(connectnet, num_games, start_idx, cpu, args, iteration):
     logger.info("[CPU: %d]: Starting MCTS self-play..." % cpu)
-    
+
     if not os.path.isdir("./datasets/iter_%d" % iteration):
         os.mkdir("./datasets/iter_%d" % iteration)
-        
+
     for idxx in tqdm(range(start_idx,num_games)):
         logger.info("[CPU: %d]: Game %d" % (cpu, idxx))
         current_board = c_board()
@@ -196,38 +196,38 @@ def MCTS_self_play(connectnet, num_games, start_idx, cpu, args, iteration):
         del dataset
         save_as_pickle("iter_%d/" % iteration +\
                        "dataset_iter%d_cpu%i_%i_%s" % (iteration, cpu, idxx, datetime.datetime.today().strftime("%Y-%m-%d")), dataset_p)
-   
+
 def run_MCTS(args, start_idx=0, iteration=0):
     net_to_play="%s_iter%d.pth.tar" % (args.neural_net_name, iteration)
     net = ConnectNet()
     cuda = torch.cuda.is_available()
     if cuda:
         net.cuda()
-    
+
     if args.MCTS_num_processes > 1:
         logger.info("Preparing model for multi-process MCTS...")
         mp.set_start_method("spawn",force=True)
         net.share_memory()
         net.eval()
-    
+
         current_net_filename = os.path.join("./model_data/",\
                                         net_to_play)
         if os.path.isfile(current_net_filename):
-            checkpoint = torch.load(current_net_filename)
+            checkpoint = torch.load(current_net_filename, map_location=torch.device('cpu'))
             net.load_state_dict(checkpoint['state_dict'])
             logger.info("Loaded %s model." % current_net_filename)
         else:
             torch.save({'state_dict': net.state_dict()}, os.path.join("./model_data/",\
                         net_to_play))
             logger.info("Initialized model.")
-        
+
         processes = []
         if args.MCTS_num_processes > mp.cpu_count():
             num_processes = mp.cpu_count()
             logger.info("Required number of processes exceed number of CPUs! Setting MCTS_num_processes to %d" % num_processes)
         else:
             num_processes = args.MCTS_num_processes
-        
+
         logger.info("Spawning %d processes..." % num_processes)
         with torch.no_grad():
             for i in range(num_processes):
@@ -237,22 +237,22 @@ def run_MCTS(args, start_idx=0, iteration=0):
             for p in processes:
                 p.join()
         logger.info("Finished multi-process MCTS!")
-    
+
     elif args.MCTS_num_processes == 1:
         logger.info("Preparing model for MCTS...")
         net.eval()
-        
+
         current_net_filename = os.path.join("./model_data/",\
                                         net_to_play)
         if os.path.isfile(current_net_filename):
-            checkpoint = torch.load(current_net_filename)
+            checkpoint = torch.load(current_net_filename, map_location=torch.device('cpu'))
             net.load_state_dict(checkpoint['state_dict'])
             logger.info("Loaded %s model." % current_net_filename)
         else:
             torch.save({'state_dict': net.state_dict()}, os.path.join("./model_data/",\
                         net_to_play))
             logger.info("Initialized model.")
-        
+
         with torch.no_grad():
             MCTS_self_play(net, args.num_games_per_MCTS_process, start_idx, 0, args, iteration)
         logger.info("Finished MCTS!")
